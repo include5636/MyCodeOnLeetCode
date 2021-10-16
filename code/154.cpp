30c8b112692decc5b2153afe027e95a1bb1a4172
include5636 <573643021@qq.com>
2021-10-16 16:55:24 +0800

    update

--- code/154.cpp
+++ code/154.cpp
@@ -6,19 +6,15 @@ public:
     int findMin(vector<int>& nums) {
         int l = 0, r = nums.size() - 1;
         while (l < r) {
-            int mid = (l + r + 1) >> 1;
-            if (nums[l] == nums[mid]) {
-                l++;
-            } else if (nums[mid] > nums[l]) {
-                l = mid; 
+            int mid = (l + r) >> 1;
+            if (nums[mid] == nums[r]) {
+                r--;
+            } else if (nums[mid] > nums[r]) {
+                l = mid + 1; 
             } else {
-                r = mid - 1;
+                r = mid;
             }
         }
-        if (l != nums.size() - 1) {
-            return nums[l + 1];
-        } else {
-            return nums[0];
-        }
+        return nums[l];
     }
 };
